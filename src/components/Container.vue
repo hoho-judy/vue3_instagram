4abdd352590843ee34342b66f43f708e1d2fb7a8
juyeonk <bpcoo99@gmail.com>
2024-07-31 11:03:30 +0900

    init

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'containerComponent',
+    components:{
+        Post: Post,
+    }
+};
+</script>
+
+<style>
+</style>
